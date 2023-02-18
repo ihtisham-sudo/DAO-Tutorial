4a1db9a7ba8392f2002898d6136ce80a84183a6c
Ihtisham Ullah <ahsaanullah174@gmail.com>
2023-02-18 13:31:42 +0500

    Contract Written

--- backend/contracts/FakeNFTMarketplace.sol
+++ backend/contracts/FakeNFTMarketplace.sol
@@ -4,7 +4,26 @@ pragma solidity ^0.8.9;
 
 contract FakeNFTMarketplace
 {
-    
+    mapping (uint256 => address) tokens;
+    uint256 nftPrice = 0.1 ether;
+    function purchase(uint256 _tokenId)  external payable
+    {
+        require(msg.value == nftPrice, "This NFT Cost 0.1 Ether");
+        tokens[_tokenId] = msg.sender;
+    } 
+    function getPrice() external view returns(uint256)
+    {
+        return nftPrice;
+    }
+    function available(uint256 _tokenId) external view returns (bool) 
+    {
+        if(tokens[_tokenId] == address(0))
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
 
 
